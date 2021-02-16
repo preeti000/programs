bf043c9cdca663d4f3ad4ac13e53f5bb56e9fe9c
preeti000 <56728816+preeti000@users.noreply.github.com>
2021-02-16 19:44:16 +0530

    Delete task3.py

--- task3.py
+++
@@ -1,9 +0,0 @@
-from collections import Counter
-import operator
-string=input("enter a string : ")
-freq=Counter(string)
-a=dict(freq)
-sorted_a=sorted(a.items(), key=operator.itemgetter(1), reverse=True)
-b=dict(sorted_a)
-for key,value in b.items():
-   print(key, ":" ,value)
